4102ccb33b8282774ce0342a59b5a9de95be7396
brownecw <brownecw@rose-hulman.edu>
2019-01-31 21:05:11 -0500

    Done with problems 1 and 2

--- src/problem5.py
+++ src/problem5.py
@@ -3,9 +3,9 @@ Exam 3, problem 5.
 
 Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher, 
          Matt Boutell, Amanda Stouder, their colleagues and 
-         PUT_YOUR_NAME_HERE.  January 2019.
+         Colin Browne.  January 2019.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -17,7 +17,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-string for the   is_prime   function defined below.
+# DONE: 2.  READ the doc-string for the   is_prime   function defined below.
 # It is the same as you have seen before.
 # After you UNDERSTAND the doc-string (JUST the doc-string, NOT the code),
 # ASKING QUESTIONS AS NEEDED, change the above _TODO_ to DONE.
@@ -145,9 +145,22 @@ def problem5(seq_of_seq):
       :rtype: (list of int) | int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    new_list = []
+    for k in range(len(seq_of_seq)):
+        current_max = [0]
+        for j in range(len(seq_of_seq[k])):
+            if seq_of_seq[k][j] > current_max[0]:
+                current_max[0] = seq_of_seq[k][j]
+        if current_max == [0]:
+            new_list = new_list + []
+        else:
+            new_list = new_list + current_max
+    return new_list
+
+
 
 
 ###############################################################################
